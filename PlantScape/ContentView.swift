c0ff7f24a70b00679fc1bf58b1212660bbf5678f
Mohammad Zhafran Dzaky <zhafrandzaky@gmail.com>
2024-05-31 09:12:06 +0700

    Initial Commit

---
+++ PlantScape/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantScape
+//
+//  Created by Mohammad Zhafran Dzaky on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantScape/PlantScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantScapeApp.swift
+//  PlantScape
+//
+//  Created by Mohammad Zhafran Dzaky on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
